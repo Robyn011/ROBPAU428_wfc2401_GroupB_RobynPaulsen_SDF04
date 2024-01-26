c00e5ccbb957f023fad1ad27daf8b1921143f707
Robyn Paulsen <149692745+Robyn011@users.noreply.github.com>
2024-01-26 16:19:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-let firstCard = Math.floor(Math.random()*10) + 2
-let secondCard = Math.floor(Math.random()*10) + 2
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true 
-let message = ""
-if (sum <= 21) {
+
+function startGame() {
+    renderGame()
+}
+
+
+function renderGame(){
+  cardsEl.textContent = "Cards: " 
+   for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+  
+  
+  sumEl.textContent = "Sum: " + sum 
+  
+  if (sum <= 20) {
   message = "Do you want to draw a new card?"
  
   }
 else if (sum === 21){
-  message = "Wohoo! You've got BlackJack!"
+  message = "You've got BlackJack!"
   hasBlackJack = True
 }
 else if (sum > 21) {
   message = "You have lost the game"
   let isAlive = false
 }
+messageEl.textContent = message
+}
 
-console.log(message) 
+function newCard() { 
+  let card = Math.floor(Math.random()*10) + 2
+ sum += card
+ cards.push(card)
+    console.log(cards)
+    renderGame()
+}
